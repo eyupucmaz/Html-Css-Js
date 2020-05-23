eba82a3932a4a78bb0e0c8b32bbf0905d57a504c
Eyup UCMAZ <eyupucmaz@gmail.com>
2020-05-23 22:17:45 +0300

    edit

---
+++ Form_Animation/app.js
@@ -0,0 +1,29 @@
+function animetedForm() {
+    const arrows = document.querySelectorAll('.fa-arrow-down');
+
+    arrows.forEach(arrow => {
+        arrow.addEventListener('click', () => {
+            const input = arrow.previousElementSibling;
+            const parent = arrow.parentElement;
+            const nextForm = parent.nextElementSibling;
+        });
+    });
+
+    function validateUser(user) {
+        if (user.value.length < 6) {
+            console.log("Not enough characters");
+            error('rgb(189,87,87)');
+        } else {
+            error('rgb(86, 189, 129)');
+        }
+    }
+
+    function error(color) {
+        document.body.style.backgroundColor = color;
+    }
+
+}
+
+
+
+animetedForm();
